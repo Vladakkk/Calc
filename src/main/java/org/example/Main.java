17fe8833fc971d074f609d9ec4e1886aae3596b6
Vladakkk <vladakasperskaa@gmail.com>
2024-05-07 17:29:28 +0300

    calculator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,45 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        double firstNum = scanner.nextDouble();
+
+        System.out.println("Enter operator ('+', '-', 'x', '/'): ");
+        String operator = scanner.next();
+
+        System.out.println("Enter second number: ");
+        double secondNum = scanner.nextDouble();
+
+        double result;
+        switch (operator){
+            case "+":
+                result = firstNum + secondNum;
+                break;
+            case  "-":
+                result = firstNum - secondNum;
+                break;
+            case "x":
+                result = firstNum * secondNum;
+                break;
+            case "/":
+                if (secondNum != 0){
+                    result = firstNum / secondNum;
+                }else{
+
+                    System.out.println("Error: division by zero!!!");
+                    return;
+                }
+                break;
+            default:
+
+                System.out.println("Error: invalid operation");
+                return;
+        }
+
+        System.out.println("Result: " + result);
     }
 }
